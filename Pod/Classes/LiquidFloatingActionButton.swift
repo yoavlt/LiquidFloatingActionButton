bcb172f1da872ce2700174ad4faaccf5ce344d22
José Enrique Sánchez Alfonso <jesanchez@jesanchez.es>
2019-03-19 19:54:11 +0100

    Updated swift 4.2

--- Pod/Classes/LiquidFloatingActionButton.swift
+++ Pod/Classes/LiquidFloatingActionButton.swift
@@ -151,7 +151,7 @@ open class LiquidFloatingActionButton : UIView {
         
         // draw plus shape
         let plusLayer = CAShapeLayer()
-        plusLayer.lineCap = kCALineCapRound
+        plusLayer.lineCap = CAShapeLayerLineCap.round
         plusLayer.strokeColor = UIColor.white.cgColor
         plusLayer.lineWidth = 3.0
         
@@ -262,9 +262,9 @@ class ActionBarBaseView : UIView {
     func translateY(_ layer: CALayer, duration: CFTimeInterval, f: (CABasicAnimation) -> ()) {
         let translate = CABasicAnimation(keyPath: "transform.translation.y")
         f(translate)
-        translate.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
+        translate.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeInEaseOut)
         translate.isRemovedOnCompletion = false
-        translate.fillMode = kCAFillModeForwards
+        translate.fillMode = CAMediaTimingFillMode.forwards
         translate.duration = duration
         layer.add(translate, forKey: "transYAnim")
     }
@@ -316,7 +316,7 @@ class CircleLiquidBaseView : ActionBarBaseView {
     func open(_ cells: [LiquidFloatingCell]) {
         stop()
         displayLink = CADisplayLink(target: self, selector: #selector(CircleLiquidBaseView.didDisplayRefresh(_:)))
-        displayLink?.add(to: RunLoop.current, forMode: RunLoopMode.commonModes)
+        displayLink?.add(to: RunLoop.current, forMode: RunLoop.Mode.common)
         opening = true
         for cell in cells {
             cell.layer.removeAllAnimations()
@@ -329,7 +329,7 @@ class CircleLiquidBaseView : ActionBarBaseView {
         stop()
         opening = false
         displayLink = CADisplayLink(target: self, selector: #selector(CircleLiquidBaseView.didDisplayRefresh(_:)))
-        displayLink?.add(to: RunLoop.current, forMode: RunLoopMode.commonModes)
+        displayLink?.add(to: RunLoop.current, forMode: RunLoop.Mode.common)
         for cell in cells {
             cell.layer.removeAllAnimations()
             cell.layer.eraseShadow()
@@ -489,7 +489,7 @@ open class LiquidFloatingCell : LiquittableCircle {
     }
     
     func setup(_ image: UIImage, tintColor: UIColor = UIColor.white) {
-        imageView.image = image.withRenderingMode(UIImageRenderingMode.alwaysTemplate)
+        imageView.image = image.withRenderingMode(UIImage.RenderingMode.alwaysTemplate)
         imageView.tintColor = tintColor
         setupView(imageView)
     }
